d22d566d00d2d13dbb40a6e739bca08b879d0c9a
D3Portillo <d3portillo@gmail.com>
2022-09-11 14:56:38 +0200

    enh(Notes.tsx): render Skeleton state when validating tx

--- components/Notes/Notes.tsx
+++ components/Notes/Notes.tsx
@@ -1,23 +1,39 @@
+import { useMemo } from "react"
+
+import useOnOffMachine from "@/lib/hooks/useOnOffMachine"
 import useNotesList from "@/lib/hooks/useNotesList"
 import NoteItem from "./NoteItem"
 import AddNote from "./AddNote"
 
 function Notes() {
   const list = useNotesList()
+  const skeleton = useOnOffMachine()
+  const $renderList = useMemo(() => {
+    const items = list.map((note) => (
+      <NoteItem {...note} key={`note-item-${note.transactionHash}`} />
+    ))
+    if (skeleton.isOn) {
+      items.push(
+        <div
+          key="mock-render-item"
+          className="bg-zinc-50 rounded-lg h-4 animate-pulse"
+        />
+      )
+    }
+    return items
+  }, [list.length, skeleton.isOn])
+
   return (
     <main className="flex-grow">
       <div className="bg-white sticky top-0 flex items-center justify-between py-4">
         <h2 className="text-xl font-bold">📒 Notes</h2>
-        <AddNote />
+        <AddNote
+          onItemCreation={skeleton.turnOn}
+          onItemValidation={skeleton.turnOff}
+        />
       </div>
       <div className="flex flex-col space-y-2">
-        {list.length ? (
-          list.map((note) => (
-            <NoteItem {...note} key={`note-item-${note.transactionHash}`} />
-          ))
-        ) : (
-          <EmptyState />
-        )}
+        {$renderList.length ? $renderList : <EmptyState />}
       </div>
     </main>
   )
